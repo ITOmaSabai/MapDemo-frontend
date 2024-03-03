422a5399491e6cd809aba8e22dd3eb18ab317a65
ItoSbai <lualaa.design2565@gmail.com>
2024-03-03 22:38:59 +0900

    add:オートコンプリート検索ボックスコンポーネントを作成

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { SelectedVideosProvider } from './contexts/SelectedVideosContext';
 import { VideosProvider } from './contexts/VideosContext';
 import { DataPostedProvider } from './contexts/DataPostedContext';
 import StreetviewPanoramaComponent from './components/StreetviewPanoramaComponent';
+import AutoCompleteComponent from './components/AutoCompleteComponent';
 
 export default function App() {
   const defaultPosition = { lat: 13.749999828728921, lng: 100.5027801676758 }
@@ -30,6 +31,7 @@ export default function App() {
                       <div style={{flex: 1}}>
                         <VideoListComponent />
                         {/* <StreetviewPanoramaComponent /> */}
+                        <AutoCompleteComponent />
                       </div>
                     </div>
                   </APIProvider>
