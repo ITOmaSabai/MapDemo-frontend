08d2df8b0e98360550902e0e2e9b4a8522e687dd
ItoSbai <lualaa.design2565@gmail.com>
2024-03-03 10:29:25 +0900

    feat:YouTube動画のフルスクリーンを許可

--- src/components/VideoListComponent.jsx
+++ src/components/VideoListComponent.jsx
@@ -10,7 +10,7 @@ const VideoListComponent = () => {
       <ul style={{margin :"0", padding: "0"}}>
         {selectedVideos.map((selectedVideo) => (
           <li key={selectedVideo.id} style={{listStyle :"none"}}>
-            <iframe width="350" height="200" src={`https://www.youtube.com/embed/${selectedVideo.youtube_video_id}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="350" height="200" src={`https://www.youtube.com/embed/${selectedVideo.youtube_video_id}`} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </li>
         ))}
       </ul>
