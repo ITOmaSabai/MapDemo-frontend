2984583538b05dd8758b938ec38f70783a993518
ItoSbai <lualaa.design2565@gmail.com>
2024-03-04 09:36:27 +0900

    add:mainブランチを反映

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { VideosProvider } from './contexts/VideosContext';
 import { DataPostedProvider } from './contexts/DataPostedContext';
 import StreetviewPanoramaComponent from './components/StreetviewPanoramaComponent';
 import { SavedMarkerProvider } from './contexts/SavedMarkerContext';
+import AutoCompleteComponent from './components/AutoCompleteComponent';
 
 export default function App() {
   const defaultPosition = { lat: 13.749999828728921, lng: 100.5027801676758 }
@@ -32,6 +33,7 @@ export default function App() {
                       <div style={{flex: 1}}>
                         <VideoListComponent />
                         {/* <StreetviewPanoramaComponent /> */}
+                        <AutoCompleteComponent />
                       </div>
                     </div>
                   </APIProvider>
