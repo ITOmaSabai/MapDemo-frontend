9b2bc0a1bcfa7276d8fe4065528acdb6787667d5
ItoSbai <lualaa.design2565@gmail.com>
2024-03-04 22:39:47 +0900

    feat:mapの表示設定を変更

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,11 +1,11 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Map, Marker } from '@vis.gl/react-google-maps';
 import SpotContext from '../contexts/SpotContext';
 import SavedMarkerComponent from './SavedMarkerComponent';
 
 const MapComponent = ({ zoom, center, onClick }) => {
   const {markers, setMarkers} = useContext(SpotContext);
-
+  
   const handleMapOnClick = (e) => {
     onClick(e);
     const lat = parseFloat(e.detail.latLng.lat);
@@ -13,8 +13,15 @@ const MapComponent = ({ zoom, center, onClick }) => {
     setMarkers({ lat, lng });
   }
   return (
-      <div style={{ height: "80vh", width: "100%" }}>
-        <Map defaultZoom={zoom} defaultCenter={center} onClick={handleMapOnClick} clickableIcons={false} >
+      <div style={{ height: "90vh", width: "100%" }}>
+        <Map
+         defaultZoom={zoom}
+         defaultCenter={center}
+         onClick={handleMapOnClick}
+         clickableIcons={false}
+         disableDefaultUI
+         gestureHandling={'greedy'}
+        >
         {markers && <Marker position={markers} />}
         {<SavedMarkerComponent />}
         </Map>
