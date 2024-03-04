7cb6a814d9751310f2eed6cc099d7a8dcd1c37d8
ItoSbai <lualaa.design2565@gmail.com>
2024-03-04 19:14:39 +0900

    feat:選択したマップのビデオをダイアログに表示する機能を追加

--- src/components/VideoDialog.jsx
+++ src/components/VideoDialog.jsx
@@ -1,26 +1,17 @@
 import * as React from 'react';
+import { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
-import PersonIcon from '@mui/icons-material/Person';
-import AddIcon from '@mui/icons-material/Add';
 import Typography from '@mui/material/Typography';
-import { blue } from '@mui/material/colors';
-import Container from '@mui/material/Container';
 import DialogContent from '@mui/material/DialogContent';
-import Stack from '@mui/material/Stack';
+import SelectedVideosContext from '../contexts/SelectedVideosContext';
 
 
 const emails = ['username@gmail.com', 'user02@gmail.com'];
 
 function SimpleDialog(props) {
+  const {selectedVideos} = useContext(SelectedVideosContext);
   const { onClose, selectedValue, open } = props;
 
   const handleClose = () => {
@@ -34,16 +25,10 @@ function SimpleDialog(props) {
   return (
     <Dialog PaperProps={{sx: {maxHeight: "95vh"}}} maxWidth={'xl'} fullWidth onClose={handleClose} open={open}>
       <DialogContent style={{height:'100vh'}}>
-        {/* <Stack  spacing={1} useFlexGap flexWrap={"wrap"}> */}
-          <iframe width="45%" height="50%" src={"https://www.youtube.com/embed/J1xWCxOKQ6A"} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          <iframe width="45%" height="50%" src={"https://www.youtube.com/embed/J1xWCxOKQ6A"} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          <iframe width="45%" height="50%" src={"https://www.youtube.com/embed/J1xWCxOKQ6A"} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          <iframe width="45%" height="50%" src={"https://www.youtube.com/embed/J1xWCxOKQ6A"} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          <iframe width="45%" height="50%" src={"https://www.youtube.com/embed/J1xWCxOKQ6A"} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          <iframe width="45%" height="50%" src={"https://www.youtube.com/embed/J1xWCxOKQ6A"} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          <iframe width="45%" height="50%" src={"https://www.youtube.com/embed/J1xWCxOKQ6A"} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          <iframe width="45%" height="50%" src={"https://www.youtube.com/embed/J1xWCxOKQ6A"} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        {/* </Stack>   */}
+        {selectedVideos && selectedVideos.length > 0 && (
+        selectedVideos.map((selectedVideo) => (
+          <iframe width="45%" height="50%" src={`https://www.youtube.com/embed/${selectedVideo.youtube_video_id}`} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        )))}
       </DialogContent>
     </Dialog>
   );
@@ -55,9 +40,10 @@ SimpleDialog.propTypes = {
   selectedValue: PropTypes.string.isRequired,
 };
 
-export default function SimpleDialogDemo() {
+export default function VideoDialog() {
   const [open, setOpen] = React.useState(false);
   const [selectedValue, setSelectedValue] = React.useState(emails[1]);
+  const {selectedVideos} = useContext(SelectedVideosContext);
 
   const handleClickOpen = () => {
     setOpen(true);
--- src/components/VideoListComponent.jsx
+++ src/components/VideoListComponent.jsx
@@ -1,20 +1,22 @@
 import React, { useContext } from 'react';
 import SelectedVideosContext from '../contexts/SelectedVideosContext';
+import VideoDialog from './VideoDialog';
 
 const VideoListComponent = () => {
   const {selectedVideos} = useContext(SelectedVideosContext);
 
   return (
     <div>
-      {selectedVideos && selectedVideos.length > 0 && (
-      <ul style={{margin :"0", padding: "0"}}>
-        {selectedVideos.map((selectedVideo) => (
-          <li key={selectedVideo.id} style={{listStyle :"none"}}>
-            <iframe width="350" height="200" src={`https://www.youtube.com/embed/${selectedVideo.youtube_video_id}`} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          </li>
-        ))}
-      </ul>
-      )}
+     {/* selectedVideos && selectedVideos.length > 0 && ( 
+       <ul style={{margin :"0", padding: "0"}}>
+         {selectedVideos.map((selectedVideo) => (
+           <li key={selectedVideo.id} style={{listStyle :"none"}}>
+             <iframe width="350" height="200" src={`https://www.youtube.com/embed/${selectedVideo.youtube_video_id}`} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+           </li>
+         ))}
+       </ul>*/}
+      <VideoDialog />
+      {/* )} */}
     </div>
   )
 }
