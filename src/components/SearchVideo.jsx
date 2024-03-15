b519cc7bc9e1684f210c2414bba09e1ca92009c2
ItoSbai <lualaa.design2565@gmail.com>
2024-03-15 10:45:01 +0900

    update:ボタンクリック後にリバースジオコードするように機能を修正

--- src/components/SearchVideo.jsx
+++ src/components/SearchVideo.jsx
@@ -29,16 +29,12 @@ const SearchVideo = () => {
 
   const getVideoSearchResult = async (resultAddress) => {
     try {
-      console.log(reverseGeocodedAddress)
-      console.log(resultAddress)
-      console.log(resultAddress.address_components)
       // const response = await fetch('https://mapdemo-backend.onrender.com/api/v1/videos/search', {
       const response = await fetch('http://localhost:3000/api/v1/videos/search', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        referrer: 'http://localhost:3001',
         body: JSON.stringify({ video: { 
           address_components: resultAddress.address_components,
           formatted_address: resultAddress.formatted_address
@@ -48,8 +44,7 @@ const SearchVideo = () => {
         throw new Error('データの送信に失敗しました');
       }
       const data = await response.json();
-      console.log(data)
-      setSearchResultVideos(data.items);
+      setSearchResultVideos(data.videos_data.items);
       setSearchedKeywords(data.search_keywords);
     } catch (error) {
       console.error('エラー:', error);
