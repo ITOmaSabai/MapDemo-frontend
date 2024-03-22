15a1d3da66d2399a2c3e453f96d38ba197d43f8f
ItoSbai <lualaa.design2565@gmail.com>
2024-03-22 23:05:55 +0900

    feat:Firebaseトークン認証機能を追加

--- src/components/SignInButton.jsx
+++ src/components/SignInButton.jsx
@@ -10,17 +10,30 @@ const SignInButton = () => {
       const token = await user?.getIdToken();
 
       const config = {
-        headers: { "Authorization": `Bearer ${token}` },
+        headers: { 'Authorization': `Bearer ${token}` },
       };
 
-      fetch('http://localhost:3000/api/v1/authentication', {
+      console.log(config.headers);
+
+      fetch(`${process.env.REACT_APP_RAILS_API_ENDPOINT}/api/v1/authentication`, {
         method: 'POST',
-        headers: config,
+        headers: config.headers,
+        // headers: {
+        //   'Authorization': `Bearer ${token}`,
+          // 'Content-Type': 'application/json'
+      // } ,
       })
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then(data => console.log(data))
-      .catch(error => console.error('Error:', error));
-    };
+      .catch(err => {
+        console.error('Error:', err);
+      });
+    }
 
   verifyIdToken();
   };
