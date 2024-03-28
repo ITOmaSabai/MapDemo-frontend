74e9cccf217a68ba4af1a5e9540d254aedd8f7dc
ItoSbai <lualaa.design2565@gmail.com>
2024-03-28 21:35:39 +0900

    feat:検索済みの場合とフィードバック画面表示済みの場合の表示画面を切り替える機能を追加

--- src/components/SearchVideo.jsx
+++ src/components/SearchVideo.jsx
@@ -11,6 +11,8 @@ import PostSpotModal from "./spotPosts/PostSpotModal";
 import { PostButton } from "./spotPosts/PostButton";
 import useFirebaseAuth from "../Hooks/useFirebasAuth";
 import MessageModal from "./Modals/MessageModal";
+import TopInfo from "./TopInfo";
+import { Feedback } from "./spotInfomations/Feedback";
 
 const SearchVideo = () => {
   const [ searchResultVideos, setSearchResultVideos ] = useState();
@@ -24,10 +26,16 @@ const SearchVideo = () => {
   const [ isVideoSearched, setIsVideoSearched ] = useState(false);
   const { currentUser } = useFirebaseAuth();
   const [ loginModalOpen, setLoginModalOpen ] = useState(false);
+  const [ isSearchedToday, setIsSearchedToday ] = useState(false);
 
   const title = "ログインすると動画を取得できます";
   const body = "街の様子をみんなにシェアしよう！"
   const icon = "✈️";
+
+  useEffect(() => {
+
+  }, [])
+  const isFeedbackSubmitted = window.sessionStorage.getItem("isFeedbackSubmitted");
 
   // ボタンを押した際のアクション
   const handleSubmit = async (e) => {
@@ -70,7 +78,7 @@ const SearchVideo = () => {
           },
         })
         const data = await response.json();
-        console.log(data)
+        setIsSearchedToday(data.isSearched);
       } catch (error) {
         console.error('エラー:', error);
       }
@@ -117,59 +125,63 @@ const SearchVideo = () => {
         body={body}
         icon={icon}
       />
-      <Paper square sx={{bgcolor: "primary.dark", height: "90vh", width:"360px", m: 0, p: 0}}>
-        <Box sx={{py: 5, md: 'flex', flexDirection: "row"}} textAlign={"center"} >
-          <Box height={"15vh"} >
-            <Typography color={"white"} fontFamily="Menlo" >
-              {markers &&
-                <ReverseGeocodingComponent />
-              }
-            </Typography>
-          </Box>
-          <Box sx={{px: 2, py: 4}} textAlign={"center"}>
-            {/* markers(クリックした地点の緯度経度)が存在すれば=マップをクリックした場合に、ボタンを表示する */}
-            {markers && !isVideoSearched ? (
-              <>
-                <Button onClick={handleSubmit} >
-                  <PostButton />
-                </Button>
-                <VideoDialog
-                  searchResultVideos={searchResultVideos}
-                  searchedKeywords={searchedKeywords}
-                  isValidAddress={isValidAddress}
-                  setIsVideoSearched={setIsVideoSearched}
-                />
-              </>
-            ) : (
-              <>
-                <Button
-                  variant="contained"
-                  color="info"
-                  type="submit"
-                  size='large'
-                  onClick={handleClickOpen}
-                >
-                  もう一度見る
-                </Button>
-                <VideoDialog
-                  searchResultVideos={searchResultVideos}
-                  searchedKeywords={searchedKeywords}
-                  isValidAddress={isValidAddress}
-                  setIsVideoSearched={setIsVideoSearched}
-                />
-              </>
-            )}
-          <Box textAlign="center" sx={{px: 2, my: 2}} >
+      {isSearchedToday ? (
+        isFeedbackSubmitted ? <TopInfo /> : <Feedback />
+      ) : (
+        <Paper square sx={{bgcolor: "primary.dark", height: "90vh", width:"360px", m: 0, p: 0}}>
+          <Box sx={{py: 5, md: 'flex', flexDirection: "row"}} textAlign={"center"} >
+            <Box height={"15vh"} >
+              <Typography color={"white"} fontFamily="Menlo" >
+                {markers &&
+                  <ReverseGeocodingComponent />
+                }
+              </Typography>
+            </Box>
+            <Box sx={{px: 2, py: 4}} textAlign={"center"}>
+              {/* markers(クリックした地点の緯度経度)が存在すれば=マップをクリックした場合に、ボタンを表示する */}
+                {markers && !isVideoSearched ? (
+                  <>
+                    <Button onClick={handleSubmit} >
+                      <PostButton />
+                    </Button>
+                    <Typography color={"white"}>１日１回まで、ピンを刺した場所の動画を見ることができます</Typography>
+                    <VideoDialog
+                      searchResultVideos={searchResultVideos}
+                      searchedKeywords={searchedKeywords}
+                      isValidAddress={isValidAddress}
+                      setIsVideoSearched={setIsVideoSearched}
+                    />
+                  </>
+                ) : (
+                  <>
+                    <Button
+                      variant="contained"
+                      color="info"
+                      type="submit"
+                      size='large'
+                      onClick={handleClickOpen}
+                    >
+                      もう一度見る
+                    </Button>
+                    <VideoDialog
+                      searchResultVideos={searchResultVideos}
+                      searchedKeywords={searchedKeywords}
+                      isValidAddress={isValidAddress}
+                      setIsVideoSearched={setIsVideoSearched}
+                    />
+                  </>
+                )}
+            <Box textAlign="center" sx={{px: 2, my: 2}} >
 
+            </Box>
           </Box>
-        </Box>
-          <Box>
-            <ConfirmSaveSpotModal searchedKeywords={searchedKeywords} open={open} />
-            <PostSpotModal />
+            <Box>
+              <ConfirmSaveSpotModal searchedKeywords={searchedKeywords} open={open} />
+              <PostSpotModal />
+            </Box>
           </Box>
-        </Box>
-      </Paper>
-
+        </Paper>
+      )}
     </>
   );
 };
